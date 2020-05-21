94a564f40fd787800ee7bbc620fb65857debe19a
Trystan Newbury trystan.newbury@gmail.com <trystan.newbury@gmail.com>
2020-05-21 18:23:17 +0100

    Make the speakerService bean a singleton

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,12 +2,15 @@ import com.conference.repository.HibernateSpeakerRepositoryImplementation;
 import com.conference.repository.SpeakerRepository;
 import com.conference.service.SpeakerService;
 import com.conference.service.SpeakerServiceImplementation;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         return new SpeakerServiceImplementation(getSpeakerRepository());
     }
